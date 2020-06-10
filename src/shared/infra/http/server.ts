2107085771eb04eb193060a82695a0271d1dd611
Geraldo Júnior <geraldojuni@hotmail.com>
2020-06-10 15:10:08 -0300

    Add Helmet

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import cors from 'cors';
+import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import { isCelebrate } from 'celebrate';
@@ -15,6 +16,7 @@ import routes from './routes';
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(routes);
 
